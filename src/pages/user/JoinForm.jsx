829d190a7cb31b5f3fb6e5853e4980d9490b29cb
황덕룡 <hwangsee12@naver.com>
2024-09-24 19:34:37 +0900

    완료

--- src/pages/user/JoinForm.jsx
+++ src/pages/user/JoinForm.jsx
@@ -1,23 +1,80 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
 const JoinForm = () => {
+    const [name, setName] = useState("");
+    const [id, setId] = useState("");
+    const [pw, setPw] = useState("");
+    const [gender, setGender] = useState('');
+  
+    const navigate = useNavigate();
+    // 이름 입력
+    let handleName = (event) => {
+        setName(event.target.value);
+    };
+
+    // 아이디 입력
+    let handleId = (event) => {
+        setId(event.target.value);
+    };
+
+    // 비밀번호 입력
+    let handlePw = (e) => {
+        setPw(e.target.value);
+    };
+
+    // 성별 체크
+    let handleGender = (e) => {
+        setGender(e.target.value);
+    };
+
+    // 폼 제출
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        // 데이터 묶기
+        const userVo = {
+            id: id,
+            password: pw,
+            name: name,
+            gender: gender,
+        };
+        
+        console.log(userVo);
+       
+        axios({
+            method: 'post',//리스트 불러오는 거는 get이었음,꼬랑지 안보임               
+            url: 'http://localhost:9000/api/users',
+            headers: { "Content-Type": "application/json; charset=utf-8" },  // post put,보낼 때 제이슨으로 보낼꺼야
+            data: userVo,     // put, post,  JSON(자동변환됨)
+            
+            responseType: 'json' //수신타입
+        }).then(response => {
+            console.log(response); //수신데이타
+            console.log(response.data);
+            if(response.data===1){
+                //리다이렉트
+                
+                navigate("/joinok");//주소말고 어디로 보낼지 잘 써놔라
+            }else
+            alert("등록실패")
+
+        }).catch(error => {
+            console.log(error);
+        });
+
+    };
+
     return (
         <>
             <div id="wrap">
-
                 <div id="header" className="clearfix">
                     <h1>
                         <Link to="">MySite</Link>
                     </h1>
 
-                    {/* 
-                    <ul>
-                        <li>황일영 님 안녕하세요^^</li>
-                        <li><a href="" className="btn_s">로그아웃</a></li>
-                        <li><a href="" className="btn_s">회원정보수정</a></li>
-                    </ul> 
-                    */}
                     <ul>
                         <li><a href="" className="btn_s">로그인</a></li>
                         <li><a href="" className="btn_s">회원가입</a></li>
@@ -62,12 +119,13 @@ const JoinForm = () => {
 
                         <div id="user">
                             <div id="joinForm">
-                                <form action="/mysite/user/join" method="get">
+                                <form action="/joinform" method="get" onSubmit={handleSubmit}>
 
                                     {/* 아이디 */}
                                     <div className="form-group">
                                         <label className="form-text" htmlFor="input-uid">아이디</label>
-                                        <input type="text" id="input-uid" name="id" placeholder="아이디를 입력하세요" />
+                                        <input type="text" id="input-uid" name="id" value={id} placeholder="아이디를 입력하세요" 
+                                        onChange={handleId} />
                                         <button type="button" id="btnIdCheck">중복체크</button>
                                     </div>
                                     <div id="message"></div>
@@ -75,22 +133,26 @@ const JoinForm = () => {
                                     {/* 비밀번호 */}
                                     <div className="form-group">
                                         <label className="form-text" htmlFor="input-pass">패스워드</label>
-                                        <input type="password" id="input-pass" name="password" placeholder="비밀번호를 입력하세요" />
+                                        <input type="password" id="input-pass" name="password" value={pw} placeholder="비밀번호를 입력하세요" 
+                                        onChange={handlePw} />
                                     </div>
 
                                     {/* 이름 */}
                                     <div className="form-group">
                                         <label className="form-text" htmlFor="input-name">이름</label>
-                                        <input type="text" id="input-name" name="name" placeholder="이름을 입력하세요" />
+                                        <input type="text" id="input-name" name="name" value={name} placeholder="이름을 입력하세요" 
+                                        onChange={handleName} />
                                     </div>
 
                                     {/* 성별 */}
                                     <div className="form-group">
                                         <span className="form-text">성별</span>
                                         <label htmlFor="rdo-male">남</label>
-                                        <input type="radio" id="rdo-male" name="gender" value="male" />
+                                        <input type="radio" id="rdo-male" name="gender" value="male" checked={gender==='male'} 
+                                        onChange={handleGender} />
                                         <label htmlFor="rdo-female">여</label>
-                                        <input type="radio" id="rdo-female" name="gender" value="female" />
+                                        <input type="radio" id="rdo-female" name="gender" value="female" checked={gender==='female'} 
+                                        onChange={handleGender}/>
                                     </div>
 
                                     {/* 약관 동의 */}
@@ -116,7 +178,7 @@ const JoinForm = () => {
                 {/* container */}
 
                 <div id="footer">
-                    Copyright ⓒ 2020 황덕룡. All right reserved
+                    Copyright ⓒ 2024 황덕룡. All right reserved
                 </div>
                 {/* footer */}
 
